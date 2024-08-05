85338a22dffa409a0f2e84b44227bcdc07694458
Naval Shah <navalmaulikshah@gmail.com>
2024-08-05 16:28:37 +0530

    PokeAPI

--- index.js
+++ index.js
@@ -12,14 +12,23 @@ fetchData();
 
 async function fetchData() {
     try{
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/mewtwo');
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
         if(!response.ok){
             throw new Error ("Could not find resource");
         }
 
         const data = await response.json();
-        console.log(data);
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite
+        imgElement.style.display = "block";
+        
+
     }
     catch(error){
         console.log(error)
